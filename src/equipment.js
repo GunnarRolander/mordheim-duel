7d010b7a81102793b1ee4fa7fe5fd2bd50c8055c
Rolander <gunnar.rolander@sweco.se>
2024-11-18 15:55:49 +0100

    Broke down app to several files (app, combat, equipment)

---
+++ src/equipment.js
@@ -0,0 +1,48 @@
+export const weapons = {
+  'handweapon': {
+      strength_mod: 0,
+      type: "slashing",
+      tags: []
+  },
+  'sword': {
+      strength_mod: 0,
+      type: "slashing",
+      tags: ['parry']
+  },
+  'club': {
+      strength_mod: 0,
+      type: "bashing",
+      tags: ['concussion']
+  },
+  'morning_star': {
+      strength_mod: 1,
+      type: "bashing",
+      tags: ['first round bonus']
+  },
+  'flail': {
+      strength_mod: 2,
+      type: "bashing",
+      tags: ['first round bonus']
+  },
+  'halberd': {
+      strength_mod: 1,
+      type: "bashing",
+      tags: []
+  },
+  'spear': {
+      strength_mod: 0,
+      type: "bashing",
+      tags: ['strike first']
+  },
+  'great weapon': {
+      strength_mod: 2,
+      type: "bashing",
+      tags: ['strike last']
+  },
+}
+
+export const armour = {
+  'buckler': {
+    tags: ['parry', 'reroll parry']
+  }
+}
